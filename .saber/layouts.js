41dacca5f91f4fd2c6e40943f49ded5a7f8df61e
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2019-12-08 00:45:49 +0530

    .saber folder "YEETED"

--- .saber/layouts.js
+++
@@ -1,8 +0,0 @@
-var layouts = {}
-
-      
-        import layout_0 from "C:/Users/ayush/Desktop/Portfolio 2.0/layouts/default.vue"
-        layouts["default"] = layout_0
-        
-
-      export default layouts
